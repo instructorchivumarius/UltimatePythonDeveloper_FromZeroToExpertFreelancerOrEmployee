a9098908d428f6d20b2534a49e3e1b26f500d53a
Chivu Marius <chivumarius@yahoo.com>
2025-07-02 10:02:01 +0200

    Tuples Data Types

---
+++ PythonBasics/Tuples.py
@@ -0,0 +1,23 @@
+# Data types: Tuples in Python
+
+# Creating a tuple
+weekdays = ("Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday")
+print("Days of the week:", weekdays)
+
+# Accessing elements
+print("First day:", weekdays[0])
+print("Last day:", weekdays[-1])
+
+# Checking length
+print("Number of days:", len(weekdays))
+
+# Single-element tuple (comma is required!)
+single_tuple = ("Python",)
+print("Single-element tuple:", single_tuple)
+
+# Iterating through a tuple
+for day in weekdays:
+    print("Day:", day)
+
+# Tuples are immutable – you can't change them
+# weekdays[0] = "Sunday"  # => will raise an error
